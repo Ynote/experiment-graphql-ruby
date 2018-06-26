8603dcb69bf937c6092154f6ba0bc4178636d8e6
Fanny Cheung <fanny@ynote.hk>
2018-06-26 21:22:06 +0200

    Udpate rate createdAt field

--- app/graphql/types/rate_type.rb
+++ app/graphql/types/rate_type.rb
@@ -4,6 +4,9 @@ RateType = GraphQL::ObjectType.define do
 
   field :uuid, !types.String, 'Unique identifier'
   field :value, types.Int, 'The value of the rate'
-  field :created_at, TimeScalar, 'The creation date of the rate'
+  field :createdAt,
+        TimeScalar,
+        'The creation date of the rate',
+        property: :created_at
   field :metric, MetricType, 'The rated metric'
 end
