49f64b0b4711902cbb11e40bc8afeaec4a294aee
Fanny Cheung <fanny@ynote.hk>
2018-06-26 20:25:33 +0200

    Assign uuid on save

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,2 +1,7 @@
 class Metric < ApplicationRecord
+  before_save :assign_uuid, on: :create
+
+  def assign_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
 end
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,3 +1,8 @@
 class Rate < ApplicationRecord
   belongs_to :metric
+  before_save :assign_uuid, on: :create
+
+  def assign_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
 end
