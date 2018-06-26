fcb9bc51348259a7137582ba6af6f7c5c69a969c
Fanny Cheung <fanny@ynote.hk>
2018-06-26 20:54:07 +0200

    Use metric uuid instead of id

--- app/graphql/mutations/rate_create_mutation.rb
+++ app/graphql/mutations/rate_create_mutation.rb
@@ -2,12 +2,13 @@ class RateCreateMutation < GraphQL::Function
   description 'Create a new rate for a metric'
 
   argument :value, !types.Int
-  argument :metric_id, !types.Int
+  argument :metric_uuid, !types.String
 
   type RateType
 
   def call(_obj, args, ctx)
-    rate = Rate.new(value: args[:value], metric_id: args[:metric_id])
+    metric = Metric.where(uuid: args[:metric_uuid]).take
+    rate = Rate.new(value: args[:value], metric: metric)
 
     rate.save!
     rate
