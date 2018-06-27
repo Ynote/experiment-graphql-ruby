9b9aec59beb3f4dbceeb8e2012fecd79d70dbe55
Fanny Cheung <fanny@ynote.hk>
2018-06-27 18:46:25 +0200

    Update RatesQuery to filter by metric

--- app/graphql/queries/rates_query.rb
+++ app/graphql/queries/rates_query.rb
@@ -3,7 +3,15 @@ class RatesQuery < GraphQL::Function
 '
   type RateType.connection_type
 
+  argument :metric_uuid, types.String
+
   def call(_obj, args, ctx)
+    if (args[:metric_uuid])
+      metric = Metric.where(uuid: args[:metric_uuid]).take
+
+      return Rate.where(metric: metric)
+    end
+
     Rate.all
   end
 end
