9056676d0fbda69ac1bb13ff89bc5ab891f44ce5
Fanny Cheung <fanny@ynote.hk>
2018-07-12 21:52:28 +0200

    Create basic chart (#1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: 'home#index'
+
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
   post "/graphql", to: "graphql#execute"
